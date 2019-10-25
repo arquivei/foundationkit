e0d20f2fd431203dba3265d2d62a33c981b6464c
Juliano Fantozzi <juliano.fantozzi@arquivei.com.br>
2019-10-25 14:38:38 -0300

    Adds unmarshaling for cUF type
    the unmarshaler also validates the cUF and
    throw errors in case of bad input

--- sefaz/cuf/uf.go
+++ sefaz/cuf/uf.go
@@ -1,6 +1,9 @@
 package cuf
 
-import "errors"
+import (
+	"encoding/json"
+	"github.com/arquivei/foundationkit/errors"
+)
 
 // CUF stands for "Codigo da unidade federativa" and it
 // is strongly associated with Stakeholder.
@@ -8,55 +11,77 @@ type CUF string
 
 // New validate and returns either (CUF, nil) if the given cUF
 // is valid or (empty CUF, error).
-func New(cUF string) (CUF, error) {
+func New(uf string) (CUF, error) {
+	cUF := CUF(uf)
 	if cUF == "" {
-		return CUF(""), errors.New("missing cUF")
+		return "", errors.New("missing cUF")
 	}
-	if isValidUF(cUF) {
-		return CUF(cUF), nil
+	if isValidUF(uf) {
+		return cUF, nil
 	}
-	return CUF(""), errors.New("invalid cUF")
+	return "", errors.New("invalid cUF")
 }
 
 func (c CUF) String() string {
 	return string(c)
 }
 
-func isValidUF(uf string) bool {
-	if len(uf) != 2 {
+//MarshalJSON serializes the CUF value as a JSON value
+func (s CUF) MarshalJSON() ([]byte, error) {
+	return json.Marshal(string(s))
+}
+
+//UnmarshalJSON deserialize a JSON value into a CUF value
+func (s *CUF) UnmarshalJSON(b []byte) error {
+	const op = errors.Op("CUF.UnmarshalJSON")
+	var v string
+
+	err := json.Unmarshal(b, &v)
+	if err != nil {
+		return errors.E(op, err)
+	}
+	if *s, err = New(v); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func isValidUF(cUF string) bool {
+	if len(cUF) != 2 {
 		return false
 	}
-	switch uf[0] {
+	switch cUF[0] {
 	case '1':
-		switch uf[1] {
+		switch cUF[1] {
 		case '0', '8', '9':
 			return false
 		default:
 			return true
 		}
 	case '2':
-		switch uf[1] {
+		switch cUF[1] {
 		case '0':
 			return false
 		default:
 			return true
 		}
 	case '3':
-		switch uf[1] {
+		switch cUF[1] {
 		case '1', '2', '3', '5':
 			return true
 		default:
 			return false
 		}
 	case '4':
-		switch uf[1] {
+		switch cUF[1] {
 		case '1', '2', '3':
 			return true
 		default:
 			return false
 		}
 	case '5':
-		switch uf[1] {
+		switch cUF[1] {
 		case '0', '1', '2', '3':
 			return true
 		default:
