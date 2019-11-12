66a8e063dfb8f253683b640e0d0fc80089c3dbbf
Gustavo Passos <gustavo.passos@arquivei.com.br>
2019-11-12 11:47:22 -0300

    CUF as a struct and add a public validation func

--- sefaz/cuf/uf_test.go
+++ sefaz/cuf/uf_test.go
@@ -1,50 +1,63 @@
 package cuf
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIsValidUFSuccess(t *testing.T) {
+func TestParseUF(t *testing.T) {
 	tests := []struct {
-		str     string
-		isValid bool
+		str    string
+		errMsg string
 	}{
-		{str: "11", isValid: true}, {str: "12", isValid: true},
-		{str: "13", isValid: true}, {str: "14", isValid: true},
-		{str: "15", isValid: true}, {str: "16", isValid: true},
-		{str: "17", isValid: true}, {str: "21", isValid: true},
-		{str: "22", isValid: true}, {str: "23", isValid: true},
-		{str: "24", isValid: true}, {str: "25", isValid: true},
-		{str: "26", isValid: true}, {str: "27", isValid: true},
-		{str: "28", isValid: true}, {str: "29", isValid: true},
-		{str: "31", isValid: true}, {str: "32", isValid: true},
-		{str: "33", isValid: true}, {str: "35", isValid: true},
-		{str: "41", isValid: true}, {str: "42", isValid: true},
-		{str: "43", isValid: true}, {str: "50", isValid: true},
-		{str: "51", isValid: true}, {str: "52", isValid: true},
-		{str: "53", isValid: true},
-
-		{str: "", isValid: false}, {str: "000", isValid: false},
-		{str: "0", isValid: false}, {str: "00", isValid: false},
-		{str: "1", isValid: false}, {str: "9", isValid: false},
-		{str: "10", isValid: false}, {str: "18", isValid: false},
-		{str: "19", isValid: false}, {str: "20", isValid: false},
-		{str: "30", isValid: false}, {str: "34", isValid: false},
-		{str: "36", isValid: false}, {str: "40", isValid: false},
-		{str: "45", isValid: false}, {str: "49", isValid: false},
-		{str: "54", isValid: false}, {str: "55", isValid: false},
-		{str: "56", isValid: false}, {str: "57", isValid: false},
-		{str: "foo", isValid: false}, {str: "bar", isValid: false},
+		{str: "11"}, {str: "12"}, {str: "13"}, {str: "14"}, {str: "15"},
+		{str: "16"}, {str: "17"}, {str: "21"}, {str: "22"}, {str: "23"},
+		{str: "24"}, {str: "25"}, {str: "26"}, {str: "27"}, {str: "28"},
+		{str: "29"}, {str: "31"}, {str: "32"}, {str: "33"}, {str: "35"},
+		{str: "41"}, {str: "42"}, {str: "43"}, {str: "50"}, {str: "51"},
+		{str: "52"}, {str: "53"},
+		{str: "", errMsg: "input cUF should have 2 digits: "},
+		{str: "000", errMsg: "input cUF should have 2 digits: 000"},
+		{str: "035", errMsg: "input cUF should have 2 digits: 035"},
+		{str: "0", errMsg: "input cUF should have 2 digits: 0"},
+		{str: "00", errMsg: "invalid cUF code: 00"},
+		{str: "1", errMsg: "input cUF should have 2 digits: 1"},
+		{str: "9", errMsg: "input cUF should have 2 digits: 9"},
+		{str: "10", errMsg: "invalid cUF code: 10"},
+		{str: "18", errMsg: "invalid cUF code: 18"},
+		{str: "19", errMsg: "invalid cUF code: 19"},
+		{str: "20", errMsg: "invalid cUF code: 20"},
+		{str: "30", errMsg: "invalid cUF code: 30"},
+		{str: "34", errMsg: "invalid cUF code: 34"},
+		{str: "36", errMsg: "invalid cUF code: 36"},
+		{str: "40", errMsg: "invalid cUF code: 40"},
+		{str: "45", errMsg: "invalid cUF code: 45"},
+		{str: "49", errMsg: "invalid cUF code: 49"},
+		{str: "54", errMsg: "invalid cUF code: 54"},
+		{str: "55", errMsg: "invalid cUF code: 55"},
+		{str: "56", errMsg: "invalid cUF code: 56"},
+		{str: "57", errMsg: "invalid cUF code: 57"},
+		{str: "foo", errMsg: "input cUF should have 2 digits: foo"},
+		{str: "bar", errMsg: "input cUF should have 2 digits: bar"},
 	}
 	for _, test := range tests {
-		assert.Equalf(t, test.isValid, isValidUF(test.str), "UF validation failed for uf %s", test.str)
+		uf, err := parseUF(test.str)
+		if test.errMsg == "" {
+			assert.NoError(t, err)
+			assert.Equal(t, test.str, strconv.Itoa(int(uf)))
+
+		} else {
+			assert.EqualError(t, err, test.errMsg)
+		}
+
 	}
 }
 
 func TestCUFString(t *testing.T) {
-	cuf := CUF("35")
+	cuf, err := New("35")
+	assert.NoError(t, err)
 	assert.Equal(t, "35", cuf.String())
 }
 
@@ -58,20 +71,26 @@ func TestNew(t *testing.T) {
 		{
 			name:     "Valid cUF",
 			input:    "35",
-			expected: CUF("35"),
+			expected: MustNew("35"),
 			err:      "",
 		},
 		{
-			name:     "Invalid cUF",
+			name:     "Wrong UF code",
 			input:    "00",
-			expected: CUF(""),
-			err:      "invalid cUF",
+			expected: CUF{},
+			err:      "cuf.New: invalid cUF code: 00",
 		},
 		{
-			name:     "Missing cUF",
-			input:    "",
-			expected: CUF(""),
-			err:      "missing cUF",
+			name:     "Not number cUF string",
+			input:    "vc",
+			expected: CUF{},
+			err:      "cuf.New: cUF could not be converted to integer: vc",
+		},
+		{
+			name:     "Not number cUF string",
+			input:    "bar",
+			expected: CUF{},
+			err:      "cuf.New: input cUF should have 2 digits: bar",
 		},
 	}
 
@@ -94,22 +113,22 @@ func TestCUF_UnmarshalJSON(t *testing.T) {
 	}{
 		{
 			data:       []byte(`"31"`),
-			expectedUF: CUF("31"),
+			expectedUF: MustNew("31"),
 			isValid:    true,
 		},
 		{
 			data:       []byte(`"banana"`),
-			expectedUF: CUF(""),
+			expectedUF: CUF{},
 			isValid:    false,
 		},
 		{
 			data:       []byte(`"30"`),
-			expectedUF: CUF(""),
+			expectedUF: CUF{},
 			isValid:    false,
 		},
 		{
 			data:       []byte(`""`),
-			expectedUF: CUF(""),
+			expectedUF: CUF{},
 			isValid:    false,
 		},
 	}
@@ -123,4 +142,34 @@ func TestCUF_UnmarshalJSON(t *testing.T) {
 		}
 		assert.Equal(t, actual, test.expectedUF, "Expected UF mismatch")
 	}
+}
+
+func TestIsValid(t *testing.T) {
+	cuf := MustNew("35")
+	assert.True(t, IsValid(cuf))
+	assert.False(t, IsValid(CUF{}))
+}
+
+func TestMustNew(t *testing.T) {
+	assert.Panics(t, func() { MustNew("blabla") })
+	assert.Equal(t, MustNew("35"), CUF{true, 35})
+}
+
+func TestString(t *testing.T) {
+	assert.Panics(t, func() {
+		d := CUF{}
+		_ = d.String()
+	}, "CUF not initialized")
+}
+
+func TestMarshalJSON_WithError(t *testing.T) {
+	cuf := CUF{}
+	_, err := cuf.MarshalJSON()
+	assert.EqualError(t, err, "CUF.MarshalJSON: CUF not initialized")
+}
+
+func TestMarshalJSON(t *testing.T) {
+	cuf := MustNew("35")
+	r, _ := cuf.MarshalJSON()
+	assert.Equal(t, []byte(`"35"`), r)
 }
