330233e21cda1e4518558906aafcc01c900597e1
Gustavo Passos <gustavo.passos@arquivei.com.br>
2019-11-12 11:47:22 -0300

    CUF as a struct and add a public validation func

--- sefaz/cuf/uf.go
+++ sefaz/cuf/uf.go
@@ -2,37 +2,58 @@ package cuf
 
 import (
 	"encoding/json"
+	"fmt"
+	"strconv"
+
 	"github.com/arquivei/foundationkit/errors"
 )
 
-// CUF stands for "Codigo da unidade federativa" and it
-// is strongly associated with Stakeholder.
-type CUF string
+// CUF stands for "Código da Unidade Federativa"
+type CUF struct {
+	initialized bool
+	value       uint8
+}
 
 // New validate and returns either (CUF, nil) if the given cUF
 // is valid or (empty CUF, error).
 func New(uf string) (CUF, error) {
-	cUF := CUF(uf)
-	if cUF == "" {
-		return "", errors.New("missing cUF")
+	const op = errors.Op("cuf.New")
+	ufInt, err := parseUF(uf)
+
+	if err != nil {
+		return CUF{}, errors.E(op, err)
 	}
-	if isValidUF(uf) {
-		return cUF, nil
+	return CUF{true, ufInt}, nil
+}
+
+// MustNew returns CUF if the given cUF is valid or panic
+func MustNew(uf string) CUF {
+	cuf, err := New(uf)
+	if err != nil {
+		panic(err)
 	}
-	return "", errors.New("invalid cUF")
+	return cuf
 }
 
 func (c CUF) String() string {
-	return string(c)
+	if !c.initialized {
+		panic("CUF not initialized")
+	}
+	return strconv.Itoa(int(c.value))
 }
 
 //MarshalJSON serializes the CUF value as a JSON value
-func (s CUF) MarshalJSON() ([]byte, error) {
-	return json.Marshal(string(s))
+func (c CUF) MarshalJSON() ([]byte, error) {
+	const op = errors.Op("CUF.MarshalJSON")
+
+	if !c.initialized {
+		return nil, errors.E(op, "CUF not initialized")
+	}
+	return json.Marshal(c.String())
 }
 
 //UnmarshalJSON deserialize a JSON value into a CUF value
-func (s *CUF) UnmarshalJSON(b []byte) error {
+func (c *CUF) UnmarshalJSON(b []byte) error {
 	const op = errors.Op("CUF.UnmarshalJSON")
 	var v string
 
@@ -40,54 +61,33 @@ func (s *CUF) UnmarshalJSON(b []byte) error {
 	if err != nil {
 		return errors.E(op, err)
 	}
-	if *s, err = New(v); err != nil {
-		return err
+	if *c, err = New(v); err != nil {
+		return errors.E(op, err)
 	}
 
 	return nil
 }
 
-func isValidUF(cUF string) bool {
+func IsValid(cUF CUF) bool {
+	return cUF.initialized
+}
+
+func parseUF(cUF string) (uint8, error) {
 	if len(cUF) != 2 {
-		return false
+		return 0, errors.Errorf("input cUF should have 2 digits: %s", cUF)
 	}
-	switch cUF[0] {
-	case '1':
-		switch cUF[1] {
-		case '0', '8', '9':
-			return false
-		default:
-			return true
-		}
-	case '2':
-		switch cUF[1] {
-		case '0':
-			return false
-		default:
-			return true
-		}
-	case '3':
-		switch cUF[1] {
-		case '1', '2', '3', '5':
-			return true
-		default:
-			return false
-		}
-	case '4':
-		switch cUF[1] {
-		case '1', '2', '3':
-			return true
-		default:
-			return false
-		}
-	case '5':
-		switch cUF[1] {
-		case '0', '1', '2', '3':
-			return true
-		default:
-			return false
-		}
+
+	ufInt64, err := strconv.ParseUint(cUF, 10, 8)
+	if err != nil {
+		return 0, errors.Errorf("cUF could not be converted to integer: %s",
+			cUF)
+	}
+
+	switch ufInt64 {
+	case 11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32,
+		33, 35, 41, 42, 43, 50, 51, 52, 53:
+		return uint8(ufInt64), nil
 	default:
-		return false
+		return 0, errors.Errorf(fmt.Sprintf("invalid cUF code: %s", cUF))
 	}
 }
