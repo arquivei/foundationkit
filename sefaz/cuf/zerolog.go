01b6d81c082273abd53dec2d5a5cc6eebfeeb8de
Gustavo Passos <gustavo.passos@arquivei.com.br>
2019-11-26 18:32:34 -0300

    Change `MarshalZerologObject` panic behavior
    When we are serializing a `CUF` we are converting it to a string. An
    empty or a nil CUF value,  therefore, should produce an empty string.

--- sefaz/cuf/zerolog.go
+++ sefaz/cuf/zerolog.go
@@ -5,6 +5,9 @@ import "github.com/rs/zerolog"
 // MarshalZerologObject implements the zerolog marshaler so it can be logged using:
 // log.With().EmbededObject(cuf).Msg("Some message")
 func (c CUF) MarshalZerologObject(e *zerolog.Event) {
-	e.
-		Str("cuf", c.String())
+	var result string
+	if c.initialized {
+		result = c.String()
+	}
+	e.Str("cuf", result)
 }
