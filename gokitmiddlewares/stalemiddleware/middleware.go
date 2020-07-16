e0c4ab84790907d9fb7c0a30ebd8339b43d58240
Rodrigo Fonseca <rodrigo.fonseca@arquivei.com.br>
2020-07-16 18:58:15 -0300

    Changes stale middleware to don't block
    Changes the stale middleware to not used any blocking mechanisms.
    
    Also refactored the code to avoid hadoukens.

--- gokitmiddlewares/stalemiddleware/middleware.go
+++ gokitmiddlewares/stalemiddleware/middleware.go
@@ -8,32 +8,46 @@ import (
 	"github.com/go-kit/kit/endpoint"
 )
 
+// New returns a new stale middleware
+// This middleware marks the application as
+// unhealthy if no request is received in
+// the allotted time.
+//
+// Unhealthy is a final state for now, so the checker
+// stops after reaching unhealthy.
+//
+// With a better health handler we could try
+// to recover from it.
 func New(c Config) endpoint.Middleware {
-	ch := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case <-ch:
-				if !app.IsHealthy() {
-					app.SetHealthy()
-				}
-			case <-time.After(c.Timeout):
-				if app.IsHealthy() {
-					if c.Logger != nil {
-						c.Logger.Warn().
-							Dur("timeout", c.Timeout).
-							Msg("Endpoint didn't receive any request and it's stale")
-					}
-					app.SetUnhealthy()
-				}
-			}
-		}
-	}()
+	var lastKnownRequestTime int64
+
+	go backgroundStaleCheck(c, &lastKnownRequestTime)
 
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (interface{}, error) {
-			ch <- struct{}{}
+			lastKnownRequestTime = time.Now().UnixNano()
 			return next(ctx, request)
 		}
 	}
+}
+
+func backgroundStaleCheck(c Config, lastKnownRequestTime *int64) {
+	time.Sleep(c.StartCheckAfter)
+	*lastKnownRequestTime = time.Now().UnixNano()
+	for {
+		time.Sleep(c.MaxTimeBetweenRequests)
+		if time.Since(time.Unix(0, *lastKnownRequestTime)) > c.MaxTimeBetweenRequests {
+			logAndSetUnhealthy(c)
+			return
+		}
+	}
+}
+
+func logAndSetUnhealthy(c Config) {
+	if c.Logger != nil {
+		c.Logger.Warn().
+			Dur("timeout", c.MaxTimeBetweenRequests).
+			Msg("Endpoint didn't receive any request and it's stale")
+	}
+	app.SetUnhealthy()
 }
