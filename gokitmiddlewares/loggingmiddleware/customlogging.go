1ff3307144cf737aa584b0efacb39857b9bf86e9
André Missaglia <andre.missaglia@arquivei.com.br>
2021-04-20 12:01:36 -0300

    Adds custom logging by duck typing
    Currently, the way to customize the log with request/response data is to set
    up a function in the config that does that.
    
    Besides the configuration overhead, this approach makes it hard to make pre-
    build resources, where minimal configuration is needed.

---
+++ gokitmiddlewares/loggingmiddleware/customlogging.go
@@ -0,0 +1,27 @@
+package loggingmiddleware
+
+import (
+	"context"
+
+	"github.com/rs/zerolog"
+)
+
+// LoggableEndpointRequest, if implemented, by the endpoint request type, will
+// be used to enrich the log INSTEAD of the middleware config.
+type LoggableEndpointRequest interface {
+	// EnrichLog enriches the current log context @zctx with the request data.
+	EnrichLog(
+		ctx context.Context,
+		zctx zerolog.Context,
+	) (context.Context, zerolog.Context)
+}
+
+// LoggableEndpointResponse, if implemented, by the endpoint response type, will
+// be used to enrich the log INSTEAD of the middleware config.
+type LoggableEndpointResponse interface {
+	// EnrichLog enriches the current log context @zctx with the response data.
+	EnrichLog(
+		ctx context.Context,
+		zctx zerolog.Context,
+	) zerolog.Context
+}
