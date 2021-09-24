e9814e0f48566ca0eb8cebab829d72f55a726497
gustavoip <gu.ip03@gmail.com>
2021-09-24 14:10:55 -0300

    Add `stringsutil` package for utility functions to handle strings (#85)
    Add `stringsutil` package for everyday utility functions to handle strings
    
    We have a bunch of projects that reimplement the same set of functions to
    handle strings. It's time to move that to here.
    
    For now, we added only the function `Truncate`.

---
+++ stringsutil/truncate.go
@@ -0,0 +1,11 @@
+package stringutils
+
+// Truncate cuts a given string if it's longer than the given size. Else it returns the string as is.
+func Truncate(str string, size int) string {
+	if len(str) <= size {
+		return str
+	}
+
+	runes := []rune(str)
+	return string(runes[:size])
+}
