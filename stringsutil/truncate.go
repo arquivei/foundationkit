5dd61c0d8baa51b8745a82b5614902ead7021696
Gustavo Passos <gustavo.passos@arquivei.com.br>
2021-09-29 10:37:33 -0300

    Package `stringsutil`'s name  is incorrectly written
    The `stringsutil` package name is causing a bit of confusion.
    This PR fixes it.

--- stringsutil/truncate.go
+++ stringsutil/truncate.go
@@ -1,4 +1,4 @@
-package stringutil
+package stringsutil
 
 // Truncate cuts a given string if it's longer than the given size. Else it returns the string as is.
 func Truncate(str string, size int) string {
