8c4146ce8984621ce20b1c9a772b937775c7c78b
gustavoip <gu.ip03@gmail.com>
2022-01-10 11:48:44 -0300

    Add gzip package for facilitated compression/decompression capabilities (#95)
    We use `gzip`  extensively for compression and decompression throughout our
    projects. But using  Golang's `compression/gzip` directly is overwhelming and
    demands lots of boilerplate code, so we created a package that wraps the native
    implementation and exposes a more straightforward API.

---
+++ gzip/gzip.go
@@ -0,0 +1,62 @@
+package gzip
+
+import (
+	"bytes"
+	"compress/gzip"
+
+	"github.com/arquivei/foundationkit/errors"
+)
+
+// Compress the @input using the gzip format
+func Compress(input []byte) ([]byte, error) {
+	const op = errors.Op("gzip.Compress")
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
+	_, err := zw.Write(input)
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	err = zw.Close()
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	return buf.Bytes(), nil
+}
+
+// MustCompress compresses @input using gzip, and panic if it fails
+func MustCompress(input []byte) []byte {
+	const op = errors.Op("gzip.MustCompress")
+	output, err := Compress(input)
+	if err != nil {
+		panic(errors.E(op, err))
+	}
+	return output
+}
+
+// Decompress @input in the gzip format
+func Decompress(input []byte) ([]byte, error) {
+	const op = errors.Op("gzip.Decompress")
+	b := bytes.NewReader(input)
+	r, err := gzip.NewReader(b)
+	if err != nil {
+		return nil, errors.E(op, err, errors.KV("step", "gzip.NewReader"))
+	}
+
+	var responseBuffer bytes.Buffer
+	_, err = responseBuffer.ReadFrom(r)
+	if err != nil {
+		return nil, errors.E(op, err, errors.KV("step", "responseBuffer.ReadFrom"))
+	}
+
+	return responseBuffer.Bytes(), nil
+}
+
+// MustDecompress decompresses @input in the gzip format, and panic if it fails
+func MustDecompress(input []byte) []byte {
+	const op = errors.Op("gzip.MustDecompress")
+	output, err := Decompress(input)
+	if err != nil {
+		panic(errors.E(op, err))
+	}
+	return output
+}
