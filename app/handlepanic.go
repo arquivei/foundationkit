2cb0f40af5a896fab280a66bbc671e17fbd76fac
Rilder Almeida <rilder.almeida@gmail.com>
2022-12-02 11:53:13 -0300

    [ FEAT ] Improves panic treatment

--- app/handlepanic.go
+++ app/handlepanic.go
@@ -8,10 +8,13 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func HandlePanic() {
-	if r := recover(); r != nil {
+// HandlePanic executes the function and, if the function panics, recovers from the panic
+// and if panic persists, logs it.
+func HandlePanic(f func()) {
+	panicErr := errors.DontPanic(f)
+	if panicErr != nil {
 		log.Fatal().
-			Err(errors.NewFromRecover(r)).
+			Err(panicErr).
 			Str("panic_stack", stringsutil.Truncate(string(debug.Stack()), 1024)).
 			Msg("App panicked!")
 	}
